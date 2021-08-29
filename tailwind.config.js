445692c6ff66a963c71a51bea0def6289969df3e
Thaís de Lima <thaislimaf@gmail.com>
2021-08-29 11:17:18 -0300

    adds fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        base: ['Red Hat Display'],
+        baseBold: ['Red Hat Display Bold'],
+      },
       colors: {
         // Configure your color palette here
         blue: {
           pale: 'hsl(225, 100%, 94%)',
           bright: 'hsl(245, 75%, 52%)',
-          verypale: 'hsl(225, 100%, 98%)',
+          veryPale: 'hsl(225, 100%, 98%)',
           desaturated: 'hsl(224, 23%, 55%)',
           dark: 'hsl(223, 47%, 23%)',
         },
